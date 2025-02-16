8b537c8e2bbf562c8d4c53374a3b5068e028c3c8
AtharvC0811 <1032222805@mitwpu.edu.in>
2025-02-16 16:12:15 +0530

    fixed LoginView

--- src/main/java/org/vaadin/example/views/LoginView.java
+++ src/main/java/org/vaadin/example/views/LoginView.java
@@ -5,19 +5,20 @@ import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.vaadin.example.model.User;
 import org.vaadin.example.service.AuthService;
 import org.vaadin.example.service.UserService;
-import com.vaadin.flow.component.notification.Notification;
 
 @Route("login")
 @PageTitle("Login | Itinerary Planner")
@@ -27,17 +28,17 @@ public class LoginView extends VerticalLayout {
     private final UserService userService;
 
     public LoginView(UserService userService) {
+
         this.userService = userService;
 
         setAlignItems(Alignment.CENTER);
 
         H1 title = new H1("Login Page");
-        title.addClassName("centered-content");
+        addClassName("centered-content");
 
         TextField usernameField = new TextField("Username");
         PasswordField passwordField = new PasswordField("Password");
         Button loginButton = new Button("Login");
-//        Button registerButton = new Button("Register");
 
         // Login Button Functionality
         loginButton.addClickListener(e -> {
@@ -59,11 +60,6 @@ public class LoginView extends VerticalLayout {
             }
         });
 
-//        // Redirect to /register
-//        registerButton.addClickListener( e -> {
-//           UI.getCurrent().navigate("register");
-//        });
-
         Div divider = new Div();
         divider.addClassName("login-divider");
 
@@ -77,15 +73,11 @@ public class LoginView extends VerticalLayout {
         // Add everything to the divider
         divider.add(registerLink, text);
 
-
         VerticalLayout formLayout = new VerticalLayout(usernameField, passwordField, loginButton, divider);
         formLayout.addClassName("login-form");
 
-        // Center content inside the VerticalLayout
-//        formLayout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        formLayout.setAlignItems(Alignment.CENTER); // Ensures form content is centered
+        formLayout.setAlignItems(Alignment.CENTER);
+
         add(title, formLayout);
-
-
     }
 }
--- src/main/java/org/vaadin/example/views/MyTripsView.java
+++ src/main/java/org/vaadin/example/views/MyTripsView.java
@@ -1,4 +1,31 @@
 package org.vaadin.example.views;
 
-public class MyTripsView {
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.vaadin.example.layout.MainLayout;
+
+import java.awt.*;
+
+
+@Route(value = "trips", layout = MainLayout.class)
+public class MyTripsView extends VerticalLayout {
+
+    public MyTripsView(){
+        H1 title = new H1("trips page");
+
+        TextField textInput = new TextField("enter name");
+
+        Button enterBtn = new Button("enter");
+        enterBtn.addClickListener(e -> {
+           textInput.getValue().trim();
+
+            System.out.println("text entered: " + textInput);
+        });
+
+
+        add(title, textInput, enterBtn);
+    }
 }
